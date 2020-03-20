6f89761080598156a47e6ae4ba9472ff03353708
Max <maxorozco213@gmail.com>
2020-03-20 15:26:12 -0700

    Implemented a search function for /discovery/v2/events endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-import superagent from 'superagent';
-import inquirer from 'inquirer';
-
+const superagent = require('superagent');
+const inquirer = require('inquirer');
+const {apiKey} = require('./apiKey');
 
 const _print = result => {
-
+    result._embedded.events.forEach(element => {
+        console.log(`${element.name} - ${element.locale}`);
+    });
+    
 };
 
 const _anotherSearchPrompt = () => {
@@ -15,3 +18,21 @@ const _anotherSearchPrompt = () => {
         }
     ]);
 };
+
+async function searchEvent(keyword = null) {
+    const baseURL = "https://app.ticketmaster.com/discovery/v2";
+    const source = "Ticketmaster";
+    let searchURL = `${baseURL}/events?apikey=${apiKey}`;
+
+    if (keyword) {
+        searchURL += `keyword=${keyword}`;
+    }
+
+    const searchResponse = await superagent.get(searchURL);
+    _print(searchResponse.body);
+}
+
+// Exporting things in ES6 syntax
+module.exports = {
+    searchEvent
+}
--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,29 @@
+const app = require('./app.js');
+const yargs = require('yargs');
+
 // Defines the commands that are accepted through the terminal
-require('yargs')
+yargs
     .usage('$0: Usage <cmd> [options]')
     .command({
-        command: "search",
+        command: 'search',
         desc: "Search the Ticketmaster API",
         // Puts the command together with some options
         builder: yargs => {
             return yargs
-                .option('a', {
-                    alias: 'all',
-                    describe: "Search all",
+                .option('k', {
+                    alias: 'keyword',
+                    describe: "Search with a keyword"
+                })
+                .option('l', {
+                    alias: 'location',
+                    describe: "Search by city name"
                 })
         },
         // Calls the function in app.js to perform some tasks
         // Function takes the options as perameters
         handler: argv => {
-            app.draw(argv)
+            app.searchEvent(argv.keyword, argv.location);
         }
     })
-    .help('help').argv;
+    .help('help')
+    .argv;
