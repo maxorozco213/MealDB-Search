ad3ca5d9ef13791cf74898c980117b1b04d1b1c7
Maximilian Orozco <maxorozco213@gmail.com>
2020-05-06 15:43:36 -0700

    Initial routes set up

--- meal-search-app/server/api/routes.js
+++ meal-search-app/server/api/routes.js
@@ -1,13 +1,31 @@
-const deckofcards = require('deckofcards');
+const foodsearch = require('foodsearch');
 const express = require('express');
 const router = express.Router();
 
 router.get('/search', async (req, res) => {
+
     
+    try {
+        const results = await foodsearch.searchByCategory(req);
+
+        res.json(results);
+    } catch {
+        console.log('error')
+    }
+    
+    console.log(results);
 });
 
-router.post('/fetch', async (req, res) => {
+router.get('/fetch', async (req, res) => {
+    try {
+        const results = await foodsearch.searchMeal(req);
+
+        res.json(results);
+    } catch {
+        console.log('error')
+    }
     
+    console.log(results);
 });
 
 module.exports = router;
