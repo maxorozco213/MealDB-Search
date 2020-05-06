f023eda7ff3b7327d0e40f839ec966d6a2412b06
Max <maxorozco213@gmail.com>
2020-05-06 14:01:30 -0700

    Set up server.js

---
+++ meal-search-app/server/api/routes.js
@@ -0,0 +1,13 @@
+const deckofcards = require('deckofcards');
+const express = require('express');
+const router = express.Router();
+
+router.get('/search', async (req, res) => {
+    
+});
+
+router.post('/fetch', async (req, res) => {
+    
+});
+
+module.exports = router;
